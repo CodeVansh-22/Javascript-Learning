263adde61d790ac9300366bdfdc8c11e6ec7ae22
CodeVansh-22 <vanshchauhand@gmail.com>
2025-08-14 12:47:23 +0000

    datatypes

---
+++ 01_Basics/datatypes2.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all js code as newer version
+
+// alert(3+3); //we are using node.js not browser, so this will not work
+console.log(3 + 3)
+console.log("Vansh")
+
+let name = "Vansh";
+let age = 20;
+let isLoggedIn = false;
+
+// Number => 2 to power 53
+// Bigint 
+// String => ""
+// Boolean => true/false
+// Null => empty value
+// Undefined => Value not assigned
+// Symbol => unique
+
+// Object
+
+console.log(typeof "Vansh");
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
