67e740351e8f32c65310e2e581540271d10c073a
CodeVansh-22 <vanshchauhand@gmail.com>
2025-08-18 11:01:18 +0000

    datatypes

--- 01_Basics/datatypes2.js
+++ 01_Basics/datatypes2.js
@@ -1,15 +1,15 @@
 "use strict"; // Treat all js code as newer version
 
 // alert(3+3); //we are using node.js not browser, so this will not work
-console.log(3 + 3)
-console.log("Vansh")
+console.log(3 + 3);
+console.log("Vansh");
 
 let name = "Vansh";
 let age = 20;
 let isLoggedIn = false;
 
 // Number => 2 to power 53
-// Bigint 
+// Bigint
 // String => ""
 // Boolean => true/false
 // Null => empty value
